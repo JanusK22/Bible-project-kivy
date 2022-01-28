0a38155ee68edccc6944cb16829e7a142289d5cb
Janusk22 <januskuete@gmail.com>
2022-01-28 23:05:43 +0100

    Ui Design: - Title on toolbar

--- test b.py
+++ test b.py
@@ -3,10 +3,18 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 
 
-class TestNavigationDrawer(MDApp):
+class Bible(MDApp):
+    data = {
+        'Version': 'book-multiple',
+        'Jump in': 'debug-step-over',
+        'Search': 'text-search',
+    }
+
+
     def build(self):
         self.theme_cls.primary_palette = "Indigo"
+
         return Builder.load_file("test.kv")
 
 
-TestNavigationDrawer().run()
+Bible().run()
